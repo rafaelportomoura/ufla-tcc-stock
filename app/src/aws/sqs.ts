53effb5b270585cc6184adbd6c36ef60d332c047
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-17 22:25:43 -0300

    test: 💍 aws/sqs

--- app/src/aws/sqs.ts
+++ app/src/aws/sqs.ts
@@ -1,14 +1,4 @@
 import {
-  DeleteMessageCommand,
-  DeleteMessageCommandInput,
-  DeleteMessageCommandOutput,
-  GetQueueAttributesCommand,
-  GetQueueAttributesCommandInput,
-  GetQueueAttributesCommandOutput,
-  QueueAttributeName,
-  ReceiveMessageCommand,
-  ReceiveMessageCommandInput,
-  ReceiveMessageCommandOutput,
   SQSClient,
   SQSClientConfig,
   SendMessageCommand,
@@ -37,47 +27,5 @@ export class SQS {
     const response = await this.client.send(command);
 
     return response;
-  }
-
-  async receiveMessage(params: Partial<ReceiveMessageCommandInput>): Promise<ReceiveMessageCommandOutput> {
-    const input = {
-      ...params,
-      QueueUrl: this.queue_url
-    };
-    const command = new ReceiveMessageCommand(input);
-
-    const response = await this.client.send(command);
-
-    return response;
-  }
-
-  async deleteMessage(receipt_handle: DeleteMessageCommandInput['ReceiptHandle']): Promise<DeleteMessageCommandOutput> {
-    const input: DeleteMessageCommandInput = {
-      QueueUrl: this.queue_url,
-      ReceiptHandle: receipt_handle
-    };
-
-    const command = new DeleteMessageCommand(input);
-
-    const response = await this.client.send(command);
-
-    return response;
-  }
-
-  async getAttributes(attribute_names: QueueAttributeName[]): Promise<GetQueueAttributesCommandOutput> {
-    const input: GetQueueAttributesCommandInput = {
-      QueueUrl: this.queue_url,
-      AttributeNames: attribute_names
-    };
-
-    const command = new GetQueueAttributesCommand(input);
-
-    const response = await this.client.send(command);
-
-    return response;
-  }
-
-  destroy(): void {
-    this.client.destroy();
   }
 }
