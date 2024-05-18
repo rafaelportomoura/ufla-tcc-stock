8f5c5d990e1dd4d9b948ae233905ac5969cc3ca6
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-17 22:26:22 -0300

    test: 💍 exceptions/Unauthorized

--- app/src/exceptions/Unauthorized.ts
+++ app/src/exceptions/Unauthorized.ts
@@ -1,10 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
-import { CODE_MESSAGES } from '../constants/codeMessages';
 import { CodeMessage } from '../types/CodeMessage';
 import { BaseError } from './BaseError';
 
 export class UnauthorizedError extends BaseError {
-  constructor(code_message: CodeMessage = CODE_MESSAGES.UNAUTHORIZED) {
+  constructor(code_message: CodeMessage) {
     super(code_message, StatusCodes.UNAUTHORIZED);
     this.name = this.constructor.name;
   }
